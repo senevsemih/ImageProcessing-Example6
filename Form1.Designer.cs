8de1d422c22336b67d3d02e3c789804176c8bf9d
senevsemih <37352722+senevsemih@users.noreply.github.com>
2019-12-27 15:53:41 +0300

    ImageProcessing

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace goruntuIslemeProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtO = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.bntDonSol = new System.Windows.Forms.Button();
+            this.Döndür = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Aquamarine;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(422, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "RESİM YÜKLE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Aquamarine;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(422, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "SEÇİLİ RENGİ TUT";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Red",
+            "Blue",
+            "Green"});
+            this.comboBox1.Location = new System.Drawing.Point(422, 122);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(28, 358);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(266, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 22);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "(A, R, G, B)";
+            // 
+            // txtO
+            // 
+            this.txtO.Location = new System.Drawing.Point(269, 410);
+            this.txtO.Name = "txtO";
+            this.txtO.Size = new System.Drawing.Size(100, 20);
+            this.txtO.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(235, 491);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 25);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Y";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(57, 492);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 25);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "X";
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(207, 519);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 20);
+            this.txtY.TabIndex = 17;
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(28, 519);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 20);
+            this.txtX.TabIndex = 16;
+            // 
+            // bntDonSol
+            // 
+            this.bntDonSol.Location = new System.Drawing.Point(396, 476);
+            this.bntDonSol.Name = "bntDonSol";
+            this.bntDonSol.Size = new System.Drawing.Size(147, 61);
+            this.bntDonSol.TabIndex = 21;
+            this.bntDonSol.Text = "Sola 90 döndür";
+            this.bntDonSol.UseVisualStyleBackColor = true;
+            this.bntDonSol.Click += new System.EventHandler(this.bntDonSol_Click);
+            // 
+            // Döndür
+            // 
+            this.Döndür.Location = new System.Drawing.Point(563, 476);
+            this.Döndür.Name = "Döndür";
+            this.Döndür.Size = new System.Drawing.Size(147, 61);
+            this.Döndür.TabIndex = 20;
+            this.Döndür.Text = "Sağa 90 döndür";
+            this.Döndür.UseVisualStyleBackColor = true;
+            this.Döndür.Click += new System.EventHandler(this.Döndür_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.ClientSize = new System.Drawing.Size(733, 569);
+            this.Controls.Add(this.bntDonSol);
+            this.Controls.Add(this.Döndür);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtO);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtO;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Button bntDonSol;
+        private System.Windows.Forms.Button Döndür;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace goruntuIslemeProjesi
+{
+    public partial class Form1 : Form
+    {
+        Bitmap resim = new Bitmap(300, 300);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+            }
+            Graphics.FromImage(resim).DrawImage(pictureBox1.Image, 0, 0, pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Color renk = new Color();
+            renk = Color.FromArgb(184, 61, 186);
+
+            for (int i = 0; i < 300; i++)
+            {
+                for (int j = 0; j < 300; j++)
+                {
+                    Color c = resim.GetPixel(i, j);
+                    if (c != renk)
+                    {
+                        resim.SetPixel(i, j, Color.Black);
+                    }
+
+                }
+            }
+            pictureBox1.Image = resim;
+            this.Refresh();
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (resim != null)
+            {
+                Color renk = resim.GetPixel(e.X, e.Y);
+                panel1.BackColor = renk;
+                txtX.Text = Convert.ToString(e.X);
+                txtY.Text = Convert.ToString(e.Y);
+
+                txtO.Text = renk.Name;
+            }
+            this.Refresh();
+        }
+
+        private void bntDonSol_Click(object sender, EventArgs e)
+        {
+            resim.RotateFlip(RotateFlipType.Rotate90FlipXY);
+            pictureBox1.Image = resim;
+        }
+
+        private void Döndür_Click(object sender, EventArgs e)
+        {
+            resim.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox1.Image = resim;
+        }
+    }
+}
